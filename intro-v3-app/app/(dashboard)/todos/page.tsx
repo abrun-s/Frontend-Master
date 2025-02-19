df61f63090fdcea72cc1c08c075155fcad6047b4
adam brunson <adam.brunson@yahoo.com>
2025-02-19 08:37:18 -0800

    Fetching Data with Prisma

--- intro-v3-app/app/(dashboard)/todos/page.tsx
+++ intro-v3-app/app/(dashboard)/todos/page.tsx
@@ -1,5 +1,17 @@
-const Todos = () => {
-  return <div>Settings</div>
+import db from '@/utils/db'
+import TodoList from '@/components/TodoList';
+
+const getData = async () => {
+  const todos = await db.todo.findMany({})
+  return todos
+}
+const TodosPage = async () => {
+  const todos = await getData()
+  return (
+  <div>
+    <TodoList todos={todos} />
+  </div>
+  )
 }
 
-export default Todos
+export default TodosPage
